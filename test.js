bbfa3c0a75c7e9d02fdb15e3bc0df0eae12870e3
ScurraAnima <33037236+ScurraAnima@users.noreply.github.com>
2019-11-11 12:11:51 +0100

    Initial commit

---
+++ test.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+var timer;
+
+function showSlides() {
+  var i;
+  { 
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active"; 
+  slides[slideIndex-1].className += " fadein"; 
+  timer = setTimeout(showSlides, 10000);
+}
+} 
+
+function stopSlide(){
+ clearTimeout (timer);
+}
+
+function resumeSlide(){
+	timer = setTimeout(showSlides, 10000);
+}
+
