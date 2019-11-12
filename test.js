c5bf0615aee273ba80723722d60035fc2a8ab96d
ScurraAnima <33037236+ScurraAnima@users.noreply.github.com>
2019-11-12 12:45:41 +0100

    Multiple Updates
    Slide fixed thanks to StackOverflow.
    Added Grid
    Added visibilityAPI for CPU usage control.
    Added Sidepanels (WIP).
    Added Dropdown Menu to Navbar.

--- test.js
+++ test.js
@@ -7,17 +7,20 @@ function showSlides() {
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.transition  = "3.0s";
+    slides[i].style.opacity = "0";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1}    
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", " ");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active"; 
-  slides[slideIndex-1].className += " fade";
-  timer = setTimeout(showSlides, 10000);
+   slides[slideIndex-1].style.transition  = "3.0s";
+   slides[slideIndex-1].style.opacity = "1";
+ 
+  dots[slideIndex-1].classList.add("active"); 
+  
+  timer = setTimeout(showSlides, 3000);
 }
 } 
 
@@ -29,7 +32,12 @@ function resumeSlide(){
 	timer = setTimeout(showSlides, 10000);
 }
 
-function fade (){
-  slides[slideIndex].className += " fadein";
-  slides[slideIndex-1].className += " fadeout";
-}
+document.addEventListener('visibilitychange', function(showSlides) {
+  if (document.hidden) {
+    stopSlide();
+    document.title = "Scramble is now paused.";
+  } else {
+    resumeSlide();
+    document.title = "Scramble";
+  }
+});
